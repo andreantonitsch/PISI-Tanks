10b797ccb5f0090369c4e5c5c876856ff2b85657
André Antonitsch <andre.antonitsch@acad.pucrs.br>
2020-04-14 23:16:19 -0300

    Initial commit.

---
+++ Assets/_Completed-Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Complete;
+
+public class InputManager : MonoBehaviour
+{
+    public Complete.GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetPlayerMovementInput(int playerID, float verticalAxis)
+    {
+        if (gameManager.m_Tanks.Length >= playerID && gameManager.m_Tanks[playerID] != null)
+            gameManager.m_Tanks[playerID].m_Movement.m_MovementInputValue = verticalAxis;
+    }
+
+    public void SetPlayerTurnInput(int playerID, float horizontalAxis)
+    {
+        if (gameManager.m_Tanks.Length >= playerID && gameManager.m_Tanks[playerID] != null)
+            gameManager.m_Tanks[playerID].m_Movement.m_TurnInputValue = horizontalAxis;
+    }
+
+    public void SetPlayerShootingPressed(int playerID, bool shooting)
+    {
+        if (gameManager.m_Tanks.Length >= playerID && gameManager.m_Tanks[playerID] != null)
+            gameManager.m_Tanks[playerID].m_Shooting.m_ShootPressed = shooting;
+    }
+
+    public void SetPlayerShootingReleased(int playerID, bool shooting)
+    {
+        if (gameManager.m_Tanks.Length >= playerID && gameManager.m_Tanks[playerID] != null)
+            gameManager.m_Tanks[playerID].m_Shooting.m_ShootReleased = shooting;
+    }
+
+    public void SetPlayerShootCharging(int playerID, bool charging)
+    {
+        if (gameManager.m_Tanks.Length >= playerID && gameManager.m_Tanks[playerID] != null)
+            gameManager.m_Tanks[playerID].m_Shooting.m_ShootCharging = charging;
+    }
+}
---
+++ Assets/_Completed-Assets/Scripts/Input/InputSource.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSource : MonoBehaviour
+{
+    public InputManager inputManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputManager.SetPlayerMovementInput(0, Input.GetAxis("Vertical1"));
+        inputManager.SetPlayerMovementInput(1, Input.GetAxis("Vertical2"));
+        inputManager.SetPlayerTurnInput(0, Input.GetAxis("Horizontal1"));
+        inputManager.SetPlayerTurnInput(1, Input.GetAxis("Horizontal2"));
+        inputManager.SetPlayerShootingPressed(0, Input.GetButtonDown("Fire1"));
+        inputManager.SetPlayerShootingPressed(1, Input.GetButtonDown("Fire2"));
+        inputManager.SetPlayerShootingReleased(0, Input.GetButtonUp("Fire1"));
+        inputManager.SetPlayerShootingReleased(1, Input.GetButtonUp("Fire2"));
+        inputManager.SetPlayerShootCharging(0, Input.GetButton("Fire1"));
+        inputManager.SetPlayerShootCharging(1, Input.GetButton("Fire2"));
+    }
+}
